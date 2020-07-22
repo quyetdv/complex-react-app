7abed5a5e37b6e1c4d6442260864d0311bca0132
quyetdv <dvquyet.utc@gmail.com>
2020-07-22 11:57:17 +0700

    first commit

---
+++ complex-react-app/app/Main.js
@@ -0,0 +1,100 @@
+import React, { useState, useReducer, useEffect } from "react"
+import ReactDom from "react-dom"
+import { useImmerReducer } from "use-immer"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
+
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessage from "./components/FlashMessage"
+import Profile from "./components/Profile"
+
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
+
+function Main() {
+  // useReducer
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexAppToken")),
+    flashMessage: [],
+    user: {
+      token: localStorage.getItem("complexAppToken"),
+      username: localStorage.getItem("complexAppUsername"),
+      avatar: localStorage.getItem("complexAppAvatar")
+    }
+  }
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "login":
+        draft.loggedIn = true
+        draft.user = action.data
+        return
+      case "logout":
+        draft.loggedIn = false
+        return
+      case "flashMessage":
+        draft.flashMessage.push(action.value)
+        return
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexAppToken", state.user.token)
+      localStorage.setItem("complexAppUsername", state.user.username)
+      localStorage.setItem("complexAppAvatar", state.user.avatar)
+    } else {
+      localStorage.removeItem("complexAppToken")
+      localStorage.removeItem("complexAppUsername")
+      localStorage.removeItem("complexAppAvatar")
+    }
+  }, [state.loggedIn])
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessage messages={state.flashMessage} />
+          <Header loggedIn={state.loggedIn} />
+          <Switch>
+            <Route path="/" exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
+            </Route>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
+            <Route path="/post/:id">
+              <ViewSinglePost />
+            </Route>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+            <Route path="/about-us">
+              <About />
+            </Route>
+            <Route path="/terms">
+              <Terms />
+            </Route>
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  )
+}
+
+ReactDom.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ complex-react-app/app/components/Profile.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react"
+
+function Profile() {
+  return <>This is profile screen</>
+}
+
+export default Profile
